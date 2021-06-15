b548d37e8a66be73757ba5e7043bd938d0bdcac7
Miklos Koren <miklos.koren@gmail.com>
2021-06-15 13:20:10 +0200

    Separate names from addresses

---
+++ projects/ventilator-procurement/README.md
@@ -0,0 +1,19 @@
+# Covid-related EU Contract Award Notices from 2020
+
+  1: uri
+  2: publication_date
+  3: buyer_name
+  4: buyer_address
+  5: buyer_town
+  6: buyer_country
+  7: cpv
+  8: amount
+  9: currency
+ 10: contractor_name
+ 11: contractor_address
+ 12: contractor_town
+ 13: contractor_country
+
+4250 rows
+
+Whenever there are more buyers or contractors, their names and addresses are separated by the `|` character.
--- projects/ventilator-procurement/read.py
+++ projects/ventilator-procurement/read.py
@@ -7,12 +7,18 @@ NMSP = {'ted': 'http://publications.europa.eu/resource/schema/ted/R2.0.9/publica
 fields = {
 	'uri': '//ted:URI_DOC',
 	'publication_date': '//ted:DATE_PUB',
-	'buyer': '//ted:CONTRACTING_BODY//text()',
+	'buyer_name': '//ted:CONTRACTING_BODY//ted:OFFICIALNAME//text()',
+	'buyer_address': '//ted:CONTRACTING_BODY//ted:ADDRESS//text()',
+	'buyer_town': '//ted:CONTRACTING_BODY//ted:TOWN//text()',
+	'buyer_country': '//ted:CONTRACTING_BODY//ted:COUNTRY//text()',
 	'cpv': '//ted:CPV_MAIN/ted:CPV_CODE/@CODE',
 	'amount': '//ted:VALUES/ted:VALUE[@TYPE="PROCUREMENT_TOTAL"]',
 	'currency': '//ted:VALUES/ted:VALUE[@TYPE="PROCUREMENT_TOTAL"]/@CURRENCY',
-	'contractors': '//ted:CONTRACTOR//text()'
-	}
+	'contractor_name': '//ted:CONTRACTOR//ted:OFFICIALNAME//text()',
+	'contractor_address': '//ted:CONTRACTOR//ted:ADDRESS//text()',
+	'contractor_town': '//ted:CONTRACTOR//ted:TOWN//text()',
+	'contractor_country': '//ted:CONTRACTOR//ted:COUNTRY//text()',
+}
 
 def list_of_xmls():
 	return glob.glob('xml/*.xml')
@@ -25,9 +31,9 @@ def parse(fn):
 			lst = doc.xpath(fields[key], namespaces=NMSP)
 			if lst:
 				try:
-					output[key] = ', '.join([str(item.text) for item in lst])
+					output[key] = '|'.join([str(item.text) for item in lst])
 				except:
-					output[key] = ', '.join([str(item) for item in lst])
+					output[key] = '|'.join([str(item) for item in lst])
 	return output
 
 if __name__ == '__main__':
